60a1f969668f84a4cac36690b81a05a8f886a39b
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-07-30 23:35:23 +0900

    coding ready

---
+++ src/components/Transform/Transform.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Transform.module.scss";
+
+export const Transform = () => {
+  return (
+    <div>
+      <div className={styles.rect} />
+    </div>
+  );
+};
