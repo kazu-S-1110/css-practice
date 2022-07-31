24829057ad631d4fe2e5f6963956e8c84762ba80
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-07-31 23:44:12 +0900

    疑似要素の復習

--- src/components/SlideButton/SlideButton.jsx
+++ src/components/SlideButton/SlideButton.jsx
@@ -5,7 +5,7 @@ import styles from "./SlideButton.module.scss";
 export const SlideButton = () => {
   return (
     <button className={classNames(styles.btn, styles.slideBg)}>
-      <span></span>SlideButton
+      SlideButton
     </button>
   );
 };
