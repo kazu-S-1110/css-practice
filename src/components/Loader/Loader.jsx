72d4c18c52ac12e9ab5e4be6d20d50ad722ed33b
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-08-04 22:42:42 +0900

    animationを任意で止められるようにした。

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useState } from "react";
 import styles from "./Loader.module.scss";
 
 export const Loader = () => {
+  const [isPause, setisPause] = useState(false);
   return (
-    <div className={styles.threeDotSpinner}>
-      <div className={styles.bounce1}></div>
-      <div className={styles.bounce2}></div>
-      <div className={styles.bounce3}></div>
-    </div>
+    <>
+      <div className={styles.threeDotSpinner}>
+        <div className={styles.bounce1}></div>
+        <div className={styles.bounce2}></div>
+        <div className={styles.bounce3}></div>
+      </div>
+      <div
+        className={classNames(styles.rect, { [styles.paused]: isPause })}
+      ></div>
+      <button onClick={() => setisPause(!isPause)}>toggle</button>
+    </>
   );
 };
