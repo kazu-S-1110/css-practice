95bd6eeb4a700dddb729b55b7cc35fa7fa13f84e
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-08-07 20:43:34 +0900

    nth-child

--- src/components/Pseudo/Pseudo.jsx
+++ src/components/Pseudo/Pseudo.jsx
@@ -4,51 +4,21 @@ import styles from "./Pseudo.module.scss";
 export const Pseudo = () => {
   return (
     <div>
-      <a href="#13">item * 1</a>
-      <a href="#23">item * 2</a>
-      <a href="#33">item * 3</a>
-      <a href="#43">item * 4</a>
-      <a href="#53">item * 5</a>
-      <a href="#63">item * 6</a>
-      <a href="#73">item * 7</a>
-      <a href="#83">item * 8</a>
-      <a href="#93">item * 9</a>
-      <a href="#103">item * 10</a>
-      <a href="#113">item * 11</a>
-      <a href="#123">item * 12</a>
-      <a href="#133">item * 13</a>
-      <a href="#143">item * 14</a>
-      <a href="#153">item * 15</a>
-      <a href="#163">item * 16</a>
-      <a href="#173">item * 17</a>
-      <a href="#183">item * 18</a>
-      <a href="#193">item * 19</a>
-      <a href="#203">item * 20</a>
-      <a href="#213">item * 21</a>
-      <a href="#223">item * 22</a>
-      <a href="#233">item * 23</a>
-      <a href="#243">item * 24</a>
-      <a href="#253">item * 25</a>
-      <a href="#263">item * 26</a>
-      <a href="#273">item * 27</a>
-      <a href="#283">item * 28</a>
-      <a href="#293">item * 29</a>
-      <a href="#303">item * 30</a>
-      <a href="#313">item * 31</a>
-      <a href="#323">item * 32</a>
-      <a href="#333">item * 33</a>
-      <a href="#343">item * 34</a>
-      <a href="#353">item * 35</a>
-      <a href="#363">item * 36</a>
-      <a href="#373">item * 37</a>
-      <a href="#383">item * 38</a>
-      <a href="#393">item * 39</a>
-      <a href="#403">item * 40</a>
-      <a href="#413">item * 41</a>
-      <a href="#423">item * 42</a>
-      <a href="#433">item * 43</a>
-      <a href="#443">item * 44</a>
-      <a href="#453">item * 45</a>
+      <a href="#1">item1</a>
+      <a href="#2">item2</a>
+      <a href="#3">item3</a>
+      <a href="#4">item4</a>
+      <a href="#5">item5</a>
+      <a href="#6">item6</a>
+      <a href="#7">item7</a>
+      <a href="#8">item8</a>
+      <a href="#9">item9</a>
+      <a href="#10">item10</a>
+      <a href="#11">item11</a>
+      <a href="#12">item12</a>
+      <a href="#13">item13</a>
+      <a href="#14">item14</a>
+      <a href="#15">item15</a>
     </div>
   );
 };
